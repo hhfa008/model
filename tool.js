1d19d4357bdbae50907cefaa3cf6a69940c480e0
hhfa <hhfa008@qq.com>
2017-11-28 16:05:07 +0800

    b

--- tool.js
+++ tool.js
@@ -5,6 +5,20 @@
 String.prototype.trim=function(){
     return this.replace(/(^\s*)|(\s*$)/g, "");
 }
+String.prototype.toTitleCase = function() {
+    var array =this.split("_")
+    var s = "";
+    array.forEach(function (t) {
+        var v  = t.charAt(0).toUpperCase() + t.substr(1)
+        if(v)
+        {
+            s+=v
+        }
+
+
+    })
+   return s
+};
 function log(d)
 {
     console.log(d);
@@ -14,9 +28,9 @@ var fs = require('fs');
 var URL = require('url');
 
 
-var src2replace = "http://ogisapi.youmoo.cn/api/"
+var src2replace = "http://www.jzhihui.com/mz/index.php"
 
-var target_path = require('os').homedir() + "/work/map/map/model/"
+var target_path = require('os').homedir() + "/git/ios2.code/Weuse/WeFun/Model/"
 
 function ReplaceFirstUper(str)
 {
@@ -29,6 +43,22 @@ function ReplaceFirstUper(str)
 fs.readFile('./src.json',"utf8",function(err,data) {
     src = data
 
+    if(src.match(/http/)){
+
+
+    } else
+    {
+        src = "http://" + src;
+    }
+
+    // var nodegrass = require('nodegrass');
+    // nodegrass.post(src,function(data,status,headers){
+    //     console.log(status);
+    //     console.log(headers);
+    //     console.log(data);
+    // },null,null,'utf8').on('error', function(e) {
+    //     console.log("Got error: " + e.message);
+    // });
     fs.readFile('./req.json',"utf8",function(err,reqdata) {
         req = reqdata
         // createQuery(req)
@@ -43,18 +73,18 @@ function json2OC(b, jsonObj) {
         for (var o in b) {
 
             if (typeof (b[o]) == 'string')
-                a = a + "\n" + ("@property (nonatomic, retain) NSString* " + (o == "id" ? "_id" : o) + ";//" + b[o])
+                a = a + "\n" + ("@property (nonatomic, copy) NSString* " + (o == "id" ? "_id" : o) + ";//" + b[o])
 
             if (b[o] instanceof Array) {
-                a = a + "\n" + ("@property (nonatomic, retain) NSArray* " + o + ";//" + b[o])
+                a = a + "\n" + ("@property (nonatomic) NSArray* " + o + ";//" + b[o])
             } else if (typeof (b[o]) == 'object')
                 a = a + "\n" + ("@property id " + o + ";//" + b[o])
 
             if (typeof (b[o]) == 'number')
-                a = a + "\n" + ("@property (nonatomic, retain) NSNumber* " + o + ";//" + b[o])
+                a = a + "\n" + ("@property (nonatomic) NSNumber* " + o + ";//" + b[o])
 
             if (typeof (b[o]) == 'boolean')
-                a = a + "\n" + ("@property (nonatomic, retain) NSNumber* " + o + ";//" + b[o])
+                a = a + "\n" + ("@property (nonatomic) NSNumber* " + o + ";//" + b[o])
         }
     a = a + "\n" + "@end"
 
@@ -162,7 +192,11 @@ function saveModel(cls, content, lastpath) {
         data = data.replace(/ClubInfoModel/g, cls);
 
         console.log(data);
-        data = data.replace("appsrc", src);
+
+
+        var url    = require('url').parse(src);
+       var reqsrc = url.path;
+        data = data.replace("appsrc", reqsrc);
         // data = data.replace(" ", src);
 
         console.log(data);
@@ -180,14 +214,44 @@ function saveModel(cls, content, lastpath) {
     })
 }
 function createItems(data,lastpath) {
-    var itmes = data.Items;
-    if (itmes) {
-        var content = ""
+
+
+    createSubItem(data,lastpath);
+}
+
+
+
+function createSubItem(data,lastpath) {
+    if (!(typeof (data) == 'string'))
+        for (var key in data) {
+
+
+            var subdata = data[key];
+            if ( typeof (subdata) == 'string')
+            {
+
+            }
+
+            else if (subdata instanceof Array) {
+                createItem(key, subdata[0]);
+
+            } else if (typeof (subdata) == 'object')
+            {
+                createItem(key, subdata);
+            }
+
+
+        }
+
+}
+function createItem(key,data,lastpath) {
+    if (key) {
+        var content = data
         try
         {
             // b = JSON.parse(b);
-            content = json2OC(itmes[0], itmes);
-            saveModel(lastpath + "ItmesModel", content, lastpath);
+            content = json2OC(data, data);
+            saveModel(key.toTitleCase() + "Model", content, lastpath);
         } catch (e)
         {
 
@@ -195,6 +259,7 @@ function createItems(data,lastpath) {
     }
 
 }
+
 function createModel(){
 fs.readFile('./data.json', "utf8",function(err,data){
     if(err) throw err;
@@ -203,19 +268,20 @@ fs.readFile('./data.json', "utf8",function(err,data){
 
     var json =jsonObj
 
-    var b = json.data  instanceof Array ? json.data[0]:json.data?json.data:json;
-    var a = ""
+    var content = json.result
+    var b = content instanceof Array ? content[0]:content?content:json;
+    var result = ""
     try
     {
         // b = JSON.parse(b);
-         a = json2OC(b, jsonObj);
-         a = "//" + req + a ;
+         result = json2OC(b, jsonObj);
+         result = "//" + req + result ;
     } catch (e)
     {
 
     }
 
-    if( json.data  instanceof Array)console.log("array\n\n")
+    if( content  instanceof Array)console.log("array\n\n")
 
     //b = b.discu[0]
 
@@ -228,15 +294,21 @@ fs.readFile('./data.json', "utf8",function(err,data){
     var arr = src.split("/")
     var lastpath = ReplaceFirstUper(arr.pop()).trim()
     lastpath = lastpath.replace(".Do","")
-    var cls =  name.length > 1 ?cls+ "Model":lastpath + "Model";
+
 
     var dcls = "ClubInfoModel"
 
 
-    src =  src.replace(src2replace,"")
-    saveModel(cls, a, lastpath);
+   // src =  src.replace(src2replace,"")
+    const querystring = require('querystring');
+   var q = querystring.parse(src)
 
-    createItems(b,lastpath);
+
+    lastpath = q.a;
+    var cls =  name.length > 1 ?cls+ "Model":lastpath + "Model";
+    cls = cls.toTitleCase();
+    saveModel(cls.toTitleCase(), result, lastpath);
+    createItems(content,lastpath);
 
 
 });
